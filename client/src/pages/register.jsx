9be7e6ef8aa720ada1725bad4ad678687fce32e9
Faruk Kurtic <farukkurtic@gmail.com>
2025-02-06 00:10:17 +0100

    add custom font

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -8,12 +8,19 @@ export default function Register() {
       <div className="left-box w-1/2  h-full flex items-center justify-center">
         <img src={doorIcon} alt="vrata-vizual" className="w-5/6 h-5/6" />
       </div>
-      <div className="right-box w-1/2 bg-orange-300 h-full">
-        <div>
+      <div className="right-box w-1/2  h-full">
+        <div className="flex items-center justify-start mt-10">
           <div>
             <img src={logo} alt="hnta-logo" className="h-40 w-40" />
           </div>
-          <div>some text</div>
+          <div className="color-white">
+            <h1 className="text-7xl font-black mb-4 tracking-wide">
+              Registruj se
+            </h1>
+            <p className="text-lg font-medium tracking-wide">
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
         </div>
       </div>
     </div>
